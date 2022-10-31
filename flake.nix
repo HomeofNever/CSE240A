f1105750b2c745b5f97c41a692d594d2e883b7e3
NeverBehave <gayhub@never.pet>
2022-10-31 15:46:04 -0700

    add environment setting

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "CSE240A Project Environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: let
+    supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+    systemOutputs = flake-utils.lib.eachSystem supportedSystems (system: let
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [];
+      };
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          editorconfig-checker
+          gcc
+          cmake
+        ];
+      };
+    });
+  in systemOutputs // {
+  };
+}
