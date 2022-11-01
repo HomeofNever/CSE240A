970681fbfbffb3d43ae255561ce980a86af4c3d7
NeverBehave <gayhub@never.pet>
2022-10-31 17:06:53 -0700

    add gshare

--- src/predictor.c
+++ src/predictor.c
@@ -12,31 +12,40 @@
 // TODO:Student Information
 //
 const char *studentName = "NAME";
-const char *studentID   = "PID";
-const char *email       = "EMAIL";
+const char *studentID = "PID";
+const char *email = "EMAIL";
 
 //------------------------------------//
 //      Predictor Configuration       //
 //------------------------------------//
 
 // Handy Global for use in output routines
-const char *bpName[4] = { "Static", "Gshare",
-                          "Tournament", "Custom" };
+const char *bpName[4] = {"Static", "Gshare",
+                         "Tournament", "Custom"};
 
+// Configuration
+// GShare
 int ghistoryBits; // Number of bits used for Global History
+// Tournament
 int lhistoryBits; // Number of bits used for Local History
 int pcIndexBits;  // Number of bits used for PC index
-int bpType;       // Branch Prediction Type
+// Others
+int bpType; // Branch Prediction Type
 int verbose;
 
 //------------------------------------//
 //      Predictor Data Structures     //
 //------------------------------------//
 
-//
-//TODO: Add your own Branch Predictor data structures here
-//
-
+// BHT
+int bht_mask = 3; // 11
+// Gshare
+int global_history_register = 0;
+int global_history_mask;
+int global_history_num;
+int *g_history;
+// Gshare shared var
+int gshare_current_index;
 
 //------------------------------------//
 //        Predictor Functions         //
@@ -44,12 +53,13 @@ int verbose;
 
 // Initialize the predictor
 //
-void
-init_predictor()
+void init_predictor()
 {
-  //
-  //TODO: Initialize Branch Predictor Data Structures
-  //
+    // Gshare
+    global_history_num = 1 << ghistoryBits;
+    global_history_mask = global_history_num - 1;
+    // FIXME: memory clean up
+    g_history = calloc(global_history_num, sizeof(int));
 }
 
 // Make a prediction for conditional branch instruction at PC 'pc'
@@ -59,33 +69,66 @@ init_predictor()
 uint8_t
 make_prediction(uint32_t pc)
 {
-  //
-  //TODO: Implement prediction scheme
-  //
-
-  // Make a prediction based on the bpType
-  switch (bpType) {
+    // Make a prediction based on the bpType
+    switch (bpType)
+    {
     case STATIC:
-      return TAKEN;
+        return TAKEN;
     case GSHARE:
+        return gshare_make_prediction(pc);
     case TOURNAMENT:
     case CUSTOM:
     default:
-      break;
-  }
+        break;
+    }
 
-  // If there is not a compatable bpType then return NOTTAKEN
-  return NOTTAKEN;
+    // If there is not a compatable bpType then return NOTTAKEN
+    return NOTTAKEN;
+}
+
+uint8_t make_prediction_gshare(uint8_t pc)
+{
+    gshare_current_index = global_history_register ^ (pc & global_history_mask);
+    if (g_history[gshare_current_index] > 1) // 00, 01, 10, 11
+        return TAKEN;
+
+    return NOTTAKEN;
 }
 
 // Train the predictor the last executed branch at PC 'pc' and with
 // outcome 'outcome' (true indicates that the branch was taken, false
 // indicates that the branch was not taken)
 //
-void
-train_predictor(uint32_t pc, uint8_t outcome)
+void train_predictor(uint32_t pc, uint8_t outcome)
 {
-  //
-  //TODO: Implement Predictor training
-  //
+    switch (bpType)
+    {
+    case GSHARE:
+        update_gshare(pc, outcome);
+        break;
+    case TOURNAMENT:
+    case CUSTOM:
+    default:
+        break;
+    }
+}
+
+// gshare
+void update_gshare(uint32_t pc, uint8_t outcome)
+{
+    // update Gshare
+    // make use of index in calculation
+    int bits = g_history[gshare_current_index];
+    bits = bits << 1;
+    bits = bits | outcome;
+    bits = bits & bht_mask;
+    g_history[gshare_current_index] = bits;
+    update_global_history_register(outcome);
+}
+
+void update_global_history_register(int outcome)
+{
+    global_history_register = global_history_register << 1;
+    global_history_register = global_history_register | outcome;
+    global_history_register = global_history_register & global_history_mask;
 }
--- src/predictor.h
+++ src/predictor.h
@@ -22,21 +22,21 @@ extern const char *email;
 //------------------------------------//
 //      Global Predictor Defines      //
 //------------------------------------//
-#define NOTTAKEN  0
-#define TAKEN     1
+#define NOTTAKEN 0
+#define TAKEN 1
 
 // The Different Predictor Types
-#define STATIC      0
-#define GSHARE      1
-#define TOURNAMENT  2
-#define CUSTOM      3
+#define STATIC 0
+#define GSHARE 1
+#define TOURNAMENT 2
+#define CUSTOM 3
 extern const char *bpName[];
 
 // Definitions for 2-bit counters
-#define SN  0			// predict NT, strong not taken
-#define WN  1			// predict NT, weak not taken
-#define WT  2			// predict T, weak taken
-#define ST  3			// predict T, strong taken
+#define SN 0 // predict NT, strong not taken
+#define WN 1 // predict NT, weak not taken
+#define WT 2 // predict T, weak taken
+#define ST 3 // predict T, strong taken
 
 //------------------------------------//
 //      Predictor Configuration       //
@@ -60,11 +60,13 @@ void init_predictor();
 // indicates a prediction of not taken
 //
 uint8_t make_prediction(uint32_t pc);
+uint8_t gshare_make_prediction(uint32_t pc);
 
 // Train the predictor the last executed branch at PC 'pc' and with
 // outcome 'outcome' (true indicates that the branch was taken, false
 // indicates that the branch was not taken)
 //
 void train_predictor(uint32_t pc, uint8_t outcome);
+void gshare_train_predictor(uint32_t pc, uint8_t outcome);
 
 #endif
